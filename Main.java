5bb380ca5a56b7256c93314d7b33b8ba5ed75fb0
Erick Pacheco <58119210+erick-pacheco@users.noreply.github.com>
2022-11-21 06:20:05 +0000

    Pass tests

--- Main.java
+++ Main.java
@@ -2,6 +2,34 @@ import java.util.*;
 
 class Main {
   public static void main(String[] args) {
+    ArrayDeque<Integer> queue = new ArrayDeque<>();
+    ArrayDeque<String> myQueue = new ArrayDeque<>();
 
+    /* Step 2: refactored */
+    int[] queItems = { 1, 2, 3, 4, 5 };
+
+    /* Step 3 */
+    for (int item : queItems) {
+      queue.offer(item);
+    }
+
+    log(queue.peek());
+
+    /* Step 4 */
+    String[] myObjs = { "Zero", "One", "Two", "Three", "Four", "Five" };
+
+    for (String item : myObjs) {
+      myQueue.offer(item);
+    }
+
+    /* Step 6 */
+    for (String item : myQueue) {
+      log(item);
+    }
+
+  }
+
+  public static void log(Object message) {
+    System.out.println(message);
   }
 }
